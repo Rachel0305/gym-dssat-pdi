974247e350f69750b7ee0d3d11fb765ca4cc4b22
Romain GAUTRON <r.gautron@cgiar.org>
2022-06-01 11:07:24 -0500

    time measure safety checks

--- execution_times.py
+++ execution_times.py
@@ -1,6 +1,7 @@
 from timeit import default_timer as timer
 import gym
 import numpy as np
+import sys
 import warnings
 import pdb
 
@@ -8,11 +9,8 @@ if __name__ == '__main__':
     humanoidstandup = gym.make('HumanoidStandup-v2')
     humanoidstandup.reset()
     env_args = {
-        'run_dssat_location': '/opt/dssat_pdi/run_dssat',
-        'log_saving_path': './logs/dssat_pdi.log',
         'mode': 'fertilization',
-        'seed': 123456,
-        'random_weather': True,
+        'seed': 123,
     }
     gymDSSAT = gym.make('gym_dssat_pdi:GymDssatPdi-v0', **env_args)
     gymDSSAT.reset()
@@ -24,34 +22,54 @@ if __name__ == '__main__':
     for repetition in range(repetitions):
         if (repetition + 1) % 100 == 0:
             print(f'repetition {repetition+1}/{repetitions}')
-        for _ in range(steps):
-            action = humanoidstandup.action_space.sample()
-            start = timer()
-            _, _, done, _ = humanoidstandup.step(action)
-            end = timer()
-            if done:
-                warnings.warn('Warning: the episode has early ended for HumanoidStandup-v2')
-            humanoidstandup_times.append(end-start)  # the time difference is in seconds by default
-        humanoidstandup.reset()
+        done = True
+        c = 0
+        while done:
+            c += 1
+            episode_steps = []
+            for _ in range(steps):
+                action = humanoidstandup.action_space.sample()
+                start = timer()
+                _, _, done, _ = humanoidstandup.step(action)
+                end = timer()
+                if done:
+                    warnings.warn('Warning: the episode has early ended for HumanoidStandup-v2')
+                episode_steps.append(end-start)  # the time difference is in seconds by default
+            if c == 100:
+                warnings.warn('Too many incomplete episodes')
+                sys.exit()
+            humanoidstandup.reset()
+        humanoidstandup_times.extend(episode_steps)
     humanoidstandup.close()
 
-    print(f'HumanoidStandup-v2 mean step time {1000 * np.mean(humanoidstandup_times)} milliseconds, std {1000 * np.std(humanoidstandup_times, ddof=1)} milliseconds')
+    print(f'\nHumanoidStandup-v2 mean step time {1000 * np.mean(humanoidstandup_times)} milliseconds, '
+          f'std {1000 * np.std(humanoidstandup_times, ddof=1)} milliseconds\n')
 
     print(f'Measuring step time for {steps} time steps, {repetitions} repetitions for GymDssatPdi-v0')
     gymDSSAT_times = []
     for repetition in range(repetitions):
         if (repetition + 1) % 100 == 0:
             print(f'repetition {repetition+1}/{repetitions}')
-        for _ in range(steps):
-            action = dict(gymDSSAT.action_space.sample())
-            action = {key: action[key].item() for key in [*action]}
-            start = timer()
-            _, _, done, _ = gymDSSAT.step(action)
-            end = timer()
-            if done:
-                warnings.warn('Warning: the episode has early ended for gymDSSAT')
-            gymDSSAT_times.append(end-start)
-        gymDSSAT.reset()
+        done = True
+        c = 0
+        while done:
+            c += 1
+            episode_steps = []
+            for _ in range(steps):
+                action = dict(gymDSSAT.action_space.sample())
+                action = {key: action[key].item() for key in [*action]}
+                start = timer()
+                _, _, done, _ = gymDSSAT.step(action)
+                end = timer()
+                if done:
+                    warnings.warn('Warning: the episode has early ended for gymDSSAT')
+                episode_steps.append(end-start)
+            if c == 100:
+                warnings.warn('Too many incomplete episodes')
+                sys.exit()
+            gymDSSAT.reset()
+        gymDSSAT_times.extend(episode_steps)
     gymDSSAT.close()
 
-    print(f'gymDSSAT mean step time {1000 * np.mean(gymDSSAT_times)} milliseconds, std {1000 * np.std(gymDSSAT_times, ddof=1)} milliseconds')
+    print(f'\ngymDSSAT mean step time {1000 * np.mean(gymDSSAT_times)} milliseconds,'
+          f' std {1000 * np.std(gymDSSAT_times, ddof=1)} milliseconds\n')
